d4196fa90af667a7f95a59b52feeaf074f0af12b
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-05-15 18:02:27 +0700

    add color to Color.kt

--- app/src/main/java/com/bigbrain/duitdoit/ui/theme/Color.kt
+++ app/src/main/java/com/bigbrain/duitdoit/ui/theme/Color.kt
@@ -2,10 +2,41 @@ package com.bigbrain.duitdoit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF2563EB)
+val PrimaryDark = Color(0xFF1D4ED8)
+val PrimaryLight = Color(0xFF3771FF)
+val Secondary = Color(0xFFEFF6FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Background = Color(0xFFFFFFFF)
+val Surface = Color(0xFFF8FAFC)
+val Border = Color(0xFFE2E8F0)
+
+val TextPrimary = Color(0xFF0F172A)
+val TextSecondary = Color(0xFF64748B)
+
+val Income = Color(0xFF16A34A)
+val Expense = Color(0xFFDC2626)
+
+val CategoryFoodDrinks = Color(0xFFEF4444)
+val CategoryTransport = Color(0xFFF97316)
+val CategoryShopping = Color(0xFFA855F7)
+val CategoryFun = Color(0xFFEC4899)
+val CategoryHealth = Color(0xFF14B8A6)
+val CategoryEducation = Color(0xFF3B82F6)
+val CategoryBills = Color(0xFFEAB308)
+val CategoryOther = Color(0xFF6B7280)
+
+val CategorySalary = Color(0xFF22C55E)
+val CategoryFreelance = Color(0xFF10B981)
+val CategoryBusiness = Color(0xFF3771FF)
+val CategoryGift = Color(0xFFEC4899)
+
+val AccountWallet = Color(0xFF2563EB)
+val AccountBank = Color(0xFF16A34A)
+val AccountEWallet = Color(0xFF8B5CF6)
+val AccountSavings = Color(0xFFEAB308)
+val AccountOther = Color(0xFF6B7280)
+
+val PriorityHigh = Color(0xFFEF4444)
+val PriorityMedium = Color(0xFFF97316)
+val PriorityLow = Color(0xFF16A34A)
